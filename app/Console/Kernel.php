32e58d182653a461592a0f42c99bf495eb5fb350
Zack Morris <zack@vynyl.com>
2017-06-28 16:11:09 -0600

    Implemented FillMySQL command to populate MySQL database with fake data.  It uses the Faker framework to populate independent tables and then populates the remaining tables that reference other tables via foreign keys.  For example `php artisan trellis:fill:mysql 10000000 locale migrations parameter question_type` adds rows until the database takes up at least 10 million bytes, skipping the `locale`, `migrations`, `parameter` and `question_type` tables.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         \App\Console\Commands\ImportSQLite::class,
+        \App\Console\Commands\FillMySQL::class,
     ];
 
     /**
