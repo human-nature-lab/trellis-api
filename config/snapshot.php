b3e3e8314c3e235de1d5cfb2f5d72a4c1a6a1114
Zack Morris <zack@vynyl.com>
2017-07-08 15:40:17 -0600

    Implemented substituting table fields on upload by specifying them in config/snapshot.php.  Currently the `device` and `user` tables are filtered out completely.

--- config/snapshot.php
+++ config/snapshot.php
@@ -7,4 +7,24 @@ return [
         'min' => env('SNAPSHOT_SECONDS_MIN', 60),
     ],
 
+    // table fields to substitute during upload/download (the * wildcard indicates all fields).  set to null to skip
+    'substitutions' => [
+        'upload' => [
+            'device' => [
+                '*' => null,
+            ],
+            'user' => [
+                '*' => null,
+            ],
+        ],
+        'download' => [
+            //TODO
+            // 'device' => [
+            //     'device_id' => null,
+            // ],
+            // 'user' => [
+            //     'password' => '',
+            // ],
+        ],
+    ],
 ];
