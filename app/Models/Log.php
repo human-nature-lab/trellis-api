10bde3986298d5bc33a8b610c06c3cfbf9ae889e
Zack Morris <zack@vynyl.com>
2017-07-10 20:09:31 -0600

    Implemented writing duplicate rows to `log` table on server.  The merge conflict strategy chooses the row that was most recently edited in UTC time, then logs the previous row (if it has not been logged previously - which prevents duplicate log entries after repeated upload syncs).  Added model events to also log any rows updated during normal REST API usage.  These events can be disabled by commenting out line 26 of Providers/EventServiceProvider.php or line 83 of bootstrap/app.php.

--- app/Models/Log.php
+++ app/Models/Log.php
@@ -2,8 +2,13 @@
 
 namespace App\Models;
 
+use App\Library\DatabaseHelper;
+use App\Services\UserService;
+use Carbon\Carbon;
+use DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class Log extends Model
 {
@@ -24,4 +29,103 @@ class Log extends Model
         'operation',
         'previous_row'
     ];
+
+    /**
+     * Create/update/delete row and append 'log' table if needed.
+     *
+     * If the optional $callback is specified, it's called with $operation set to one of ['create', 'update', 'delete'] to allow the caller to handle the write manually.
+     *
+     * @var array $row Key-value pairs representing database row to update/delete
+     * @var array $table Database table to use
+     * @var function $callback Callback of the form "function callback($row, $table, $operation) {}" (where $operation is 'create', 'update' or 'delete') for overriding database write, pass null to write row automatically
+     * @return string|null One of 'create', 'update', 'delete' (meaning row was newer than previous row), or null if it was logged
+     */
+    public static function writeRow($row, $table, $callback = null)
+    {
+        return DB::transaction(function () use ($row, $table, $callback) {
+            if ($table == (new self)->table) {
+                return; // don't log writes involving log table
+            }
+
+            $userId = UserService::getCurrentUserId();
+
+            if (!$userId) {
+                return; //TODO decide how to handle database updates when no authorized user (should never happen)
+            }
+
+            $operation = null;
+            $previousRow = (array) DB::table($table)->where('id', $row['id'])->first();
+
+            if ($previousRow) {
+                if ($previousRow != $row) {
+                    $previousTimestamp = DatabaseHelper::modifiedAt($previousRow);
+                    $timestamp = DatabaseHelper::modifiedAt($row);
+
+                    if ($previousTimestamp < $timestamp) {
+                        $operation = (array_get($row, 'deleted_at') && !array_get($previousRow, 'deleted_at')) ? 'delete' : 'update';
+                    } else {
+                        $previousRow = $row;    // if row is older than previous row, then swap them to log it instead
+                    }
+
+                    $log = [
+                        'actor_id' => $userId,
+                        'row_id' => $row['id'],
+                        'table_name' => $table,
+                        'operation' => $operation,
+                        'previous_row' => json_encode($previousRow, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES),
+                    ];
+
+                    if (!static::where($log)->first()) {
+                        $log['id'] = Uuid::uuid4();
+
+                        static::create($log);    // insert log entry if it's not already present
+                    }
+                }
+            } else {
+                $operation = 'create';
+            }
+
+            if ($operation) {
+                if ($callback) {
+                    $callback($row, $table, $operation);
+                } else {
+                    if ($operation == 'create') {
+                        DB::table($table)->insert($row);
+                    } else {
+                        DB::table($table)->where('id', $row['id'])->update($row);
+                    }
+                }
+
+                return $operation;
+            }
+
+            return null;
+        });
+    }
+
+    public static function onModelUpdating($model)
+    {
+        DB::beginTransaction();
+
+        $row = $model->getAttributes();
+
+        // force model write to succeed by using most-future timestamp for comparison
+        $row[$model->getCreatedAtColumn()] = Carbon::createFromTimeStampUTC(PHP_INT_MAX);
+        $row[$model->getUpdatedAtColumn()] = Carbon::createFromTimeStampUTC(PHP_INT_MAX);
+
+        if (strpos(\Event::firing(), 'eloquent.deleting:') === 0) {
+            $deletedAtColumn = in_array(SoftDeletes::class, class_uses($model)) ? $model->getDeletedAtColumn() : 'deleted_at';
+
+            $row[$deletedAtColumn] = Carbon::createFromTimeStampUTC(PHP_INT_MAX);  // indicate that row is being deleted (model will perform actual delete afterwards)
+        }
+
+        static::writeRow($row, $model->getTable(), function ($row, $table, $operation) {
+            // skip database write and let model handle it
+        });
+    }
+
+    public static function onModelUpdated($model)
+    {
+        DB::commit();
+    }
 }
