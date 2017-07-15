d49314fffb3fe3abb03860eb9b9a9499d86316e4
Zack Morris <zack@vynyl.com>
2017-07-14 19:36:55 -0600

    Implemented ExecutableTest to verify that gzip, mysql, mysql2sqlite and mysqldump are installed and executable.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -11,4 +11,21 @@ abstract class TestCase extends Laravel\Lumen\Testing\TestCase
     {
         return require __DIR__.'/../bootstrap/app.php';
     }
+
+    protected function assertExecutableExists($executable, $command = null, $message = null)
+    {
+        if (is_null($command)) {
+            $command = "$executable --help";
+        }
+
+        exec($command, $output, $exitCode);
+
+        if (is_null($message)) {
+            $message = "Could not find the executable `$executable`, please install it.";
+        }
+
+        $this->assertEquals(
+            0, $exitCode, $message
+        );
+    }
 }
