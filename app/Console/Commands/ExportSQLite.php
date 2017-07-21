a7798636788f66079867c1efbadc5bab50c339aa
Zack Morris <zack@vynyl.com>
2017-07-21 16:46:16 -0600

    Implemented converting ...\n... to ...' || x'0A' || '... within SQL values in `php artisan trellis:export:sqlite` to allow for easier splitting of statements on ";\n" by the client.

--- app/Console/Commands/ExportSQLite.php
+++ app/Console/Commands/ExportSQLite.php
@@ -57,8 +57,34 @@ class ExportSQLite extends Command
             $dumpPathString = '';
         }
 
+        /*
+
+        SQLite uses PIPES_AS_CONCAT SQL mode instead of escaping the linefeed character, which means that insert statements cross line boundaries.
+        This script converts ...\n... to ...' || x'0A' || '... to allow for easier splitting of statements on ";\n" by the client.
+        The conversion is then passed to mysql2sqlite, which supports PIPES_AS_CONCAT mode for input and passes it through.
+        Note that '\n' is ASCII [92 110], not the "\n" linefeed LF character [10].  '\n' will never appear outside of field values in the SQL dump.
+        Here are some variations of converting a 100 MB SQL dump on a 2.2 GHz Intel Core i7 for comparison (sed and awk need optimization):
+
+        $ time php artisan trellis:export:mysql > /dev/null
+        real	0m1.769s
+
+        $ time php artisan trellis:export:mysql | php -r 'while($s = fgets(STDIN)) echo preg_replace("/(?<=[^\\\\])(\\\\{2})*\\\\n/", "'\'' || x'\''0A'\'' || '\''", $s);' > /dev/null
+        real	0m2.114s
+
+        $ time php artisan trellis:export:mysql | perl -ne "s/(?<=[^\\\\])(\\\\{2})*\\\\n/' || x'0A' || '/g; print;" > /dev/null
+        real	0m2.297s
+
+        $ time php artisan trellis:export:mysql | sed 's/\([^\\]\)\(\\\{2\}\)*\\n/\1'\'' || x'\''0A'\'' || '\''/g' > /dev/null
+        real	0m11.539s
+
+        $ time php artisan trellis:export:mysql | awk '{ print match($0, /(.*)([^\\])(\\{2})*\\n(.*)/, a) ? a[1]a[2]"'"' || x'0A' || '"'"a[4] : $0 }' > /dev/null
+        real	11m9.513s
+
+         */
         $process = new Process(<<<EOT
-php artisan trellis:export:mysql $excludeTablesString | $mysql2sqlite - $dumpPathString
+php artisan trellis:export:mysql $excludeTablesString |
+php -r 'while(\$s = fgets(STDIN)) echo preg_replace("/(?<=[^\\\\\\\\])(\\\\\\\\{2})*\\\\\\\\n/", "'\'' || x'\''0A'\'' || '\''", \$s);' |
+$mysql2sqlite - $dumpPathString
 EOT
 , base_path());
 
