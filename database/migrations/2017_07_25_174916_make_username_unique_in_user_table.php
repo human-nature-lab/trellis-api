d60e0931248ec072a753efb6a532fa0769bb8d0d
Zack Morris <zack@vynyl.com>
2017-07-25 18:42:16 -0600

    Added migration to make user.username unique.

---
+++ database/migrations/2017_07_25_174916_make_username_unique_in_user_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class MakeUsernameUniqueInUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->unique(['username']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->dropUnique(['username']);
+        });
+    }
+}
