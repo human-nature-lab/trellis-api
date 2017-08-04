753f01bf02393f6b15cf2c4df3c810fd76e0748d
Zack Morris <zack@vynyl.com>
2017-08-04 13:18:43 -0600

    Added "log" table to upload sync skips.  It's skipped internally by Log::writeRow() so just adding it for clarity.

--- config/snapshot.php
+++ config/snapshot.php
@@ -24,6 +24,9 @@ return [
             'key' => [
                 '*' => null,
             ],
+            'log' => [
+                '*' => null,
+            ],
             'migrations' => [
                 '*' => null,
             ],
