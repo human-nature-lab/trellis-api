49c824c1bda462fd0372b7e003f6c9f2f82c353b
Zack Morris <zack@vynyl.com>
2017-08-07 16:36:47 -0600

    Updated database seeders for new schema. Use by calling `php artisan migrate --seed`.

---
+++ database/seeds/sql/device.sql
@@ -0,0 +1,3 @@
+INSERT INTO `device` (`id`, `device_id`, `name`, `epoch`, `created_at`, `updated_at`, `deleted_at`)
+VALUES
+	('00000000-0000-0000-0000-000000000000','0000000000000000','Test Device',0,'2017-06-21 00:39:53','2017-08-07 19:54:01',NULL);
