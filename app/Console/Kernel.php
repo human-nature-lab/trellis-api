bcc4606cca17b45c88d0d1d6c2dfd2e5d2a0fcb7
Zack Morris <zack@vynyl.com>
2017-09-08 16:39:20 -0600

    Implemented `php artisan trellis:show:mysql:foreignkeycycles` to print JSON representation of foreign key traversals resulting in cycles. Format is [table1: [column1: [table2 : [column2: ...table1]]]].

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,6 +28,7 @@ class Kernel extends ConsoleKernel
         \App\Console\Commands\MergeMigrations::class,
         \App\Console\Commands\ShowMySQLJSON::class,
         \App\Console\Commands\ShowMySQLForeignKeys::class,
+        \App\Console\Commands\ShowMySQLForeignKeyCycles::class,
         \App\Console\Commands\SimulateMigrate::class,
         \App\Console\Commands\SimulateMigrateRollback::class,
         \App\Console\Commands\ToTable::class,
