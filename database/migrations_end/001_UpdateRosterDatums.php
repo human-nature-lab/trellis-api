b0757c7ca0daf14bf818da28d20c9edea311c391
Zack Morris <zack@vynyl.com>
2017-10-12 21:13:18 -0600

    Implemented migration to convert existing roster datums to use a primary datum per roster with each roster item's datum.parent_datum_id set to the primary roster datum's id.

---
+++ database/migrations_end/001_UpdateRosterDatums.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Database\MigrationsEnd;
+
+use DB;
+use Illuminate\Console\Command;
+
+//TODO remove this command once every user has Android Trellis version 2.1.4 or higher.  until then, run `php artisan migrate ` periodically
+class UpdateRosterDatums extends Command
+{
+    // /**
+    //  * The name and signature of the console command.
+    //  *
+    //  * @var string
+    //  */
+    protected $signature = 'Not registered in app/Console/Kernel.php';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        DB::transaction(function () {
+            // create datums with val = roster_parent for any roster questions that have been answered using the old system
+            DB::unprepared(<<<'EOT'
+insert into datum (
+	id,
+	name,
+	val,
+	choice_id,
+	survey_id,
+	question_id,
+	repetition,
+	parent_datum_id,
+	datum_type_id,
+	sort_order,
+	created_at,
+	updated_at,
+	deleted_at
+)
+select distinct
+	(select uuid()),
+	'roster_variable',
+	'roster_parent',
+	null,
+	datum.survey_id,
+	question.id,
+	0,
+	null,
+	0,
+	null,
+	(select now()),
+	(select now()),
+	null
+from question
+join datum on question.id = datum.question_id
+and datum.parent_datum_id is null
+and datum.val != 'roster_parent'
+and question.question_type_id = (select id from question_type where name = 'roster' limit 1)
+and question.id not in (
+	select distinct question.id
+	from question
+	join datum on question.id = datum.question_id
+	and datum.parent_datum_id is null
+	and datum.val = 'roster_parent'
+	and question.question_type_id = (select id from question_type where name = 'roster' limit 1)
+);
+EOT
+            );
+
+            // set roster question child datum parent_datum_id to parent datums
+            DB::unprepared(<<<'EOT'
+update datum
+join question on datum.question_id = question.id and question.question_type_id = (select id from question_type where name = 'roster' limit 1)
+join datum as parent_datum on datum.survey_id = parent_datum.survey_id and datum.question_id = parent_datum.question_id and parent_datum.parent_datum_id is null and parent_datum.val = 'roster_parent' and datum.parent_datum_id is null and datum.val != 'roster_parent'
+set datum.parent_datum_id = parent_datum.id;
+EOT
+            );
+        });
+    }
+}
