076a7c8c71381ab0bb291a15f9b62ec7e3660488
Zack Morris <zack@vynyl.com>
2017-12-12 12:13:43 -0700

    Created `preload` table. Added `preload_id` and made `survey_id` nullable in `datum` table.

---
+++ app/Models/Preload.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Preload extends Model
+{
+    use SoftDeletes;
+
+    public $incrementing = false;
+
+    protected $table = 'preload';
+
+    protected $fillable = [
+        'id',
+        'respondent_id',
+        'form_id',
+        'study_id',
+        'last_question_id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'completed_at'
+    ];
+
+    /*
+    public function delete()
+    {
+        $childDatum = Datum::where('survey_id', '=', $this->id)->get();
+        foreach ($childDatum as $childData) {
+            $childData->delete();
+        }
+
+        return parent::delete();
+    }
+    */
+}
