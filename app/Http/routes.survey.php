4987e0c2e5b7c022f6e35f45fca7d6bbf17d17f2
wyattis <wyattisrael@gmail.com>
2018-04-18 15:18:51 -0400

    Broke routes into two files. Added route prefix for all survey administration routes and a view to serve as a placeholder for the vue application.

---
+++ app/Http/routes.survey.php
@@ -0,0 +1,16 @@
+<?php
+
+//***************************//
+//* Token Controller Routes *//
+//***************************//
+
+
+use Illuminate\Support\Facades\Artisan;
+
+
+$app->group([
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'survey-view'
+], function ($app) {
+    $app->get('form/{form_id}', 'SurveyViewController@showLogin');
+});
