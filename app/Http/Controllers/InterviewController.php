5cc7ff576060b0d07ee30e62ce7e8120e0874aa7
Mark McKnight <mark.mcknight@yale.edu>
2018-04-19 12:13:01 -0400

    Fixed InterviewerControlelr.submit() to work with new question_datum schema. Added migration to make question_datum.follow_up_datum_id nullable.

--- app/Http/Controllers/InterviewController.php
+++ app/Http/Controllers/InterviewController.php
@@ -4,13 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Models\Edge;
 use App\Models\EdgeDatum;
-use App\Models\Datum;
 use App\Models\Interview;
 use App\Models\Respondent;
 use App\Models\SelfAdministeredSurvey;
 use App\Models\Survey;
 use App\Models\User;
 use App\Models\Token;
+use App\Services\DatumService;
 use Laravel\Lumen\Routing\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -26,7 +26,7 @@ class InterviewController extends Controller
      * @param Request $request
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function submit(Request $request)
+    public function submit2(Request $request)
     {
         $validator = Validator::make($request->all(), [
             'study_id' => 'required|string|min:36|exists:study,id',
@@ -71,6 +71,83 @@ class InterviewController extends Controller
         ], Response::HTTP_OK);
     }
 
+    public function submit(Request $request, DatumService $datumService) {
+
+        $validator = Validator::make($request->all(), [
+            'study_id' => 'required|string|min:36|exists:study,id',
+            'form_id' => 'required|string|min:36|exists:form,id',
+            'respondent_id' => 'required|string|min:36|exists:respondent,id',
+            'token_id' => 'required|string|min:36|exists:token,id',
+            'questions' => 'required|array'
+        ]);
+
+        if ($validator->fails() === true) {
+            return response()->json([
+                'msg' => 'Validation failed',
+                'err' => $validator->errors()
+            ], $validator->statusCode());
+        }
+
+        $token = Token::find($request->input('token_id'));
+        $user = User::find($token->user_id);
+        $surveyModel = new Survey;
+        $interviewModel = new Interview;
+
+        DB::transaction(function() use ($request, $surveyModel, $interviewModel, $user, $datumService) {
+
+            $surveyId = Uuid::uuid4();
+            $interviewId = Uuid::uuid4();
+            $now = new DateTime();
+
+            // Create new Survey.
+            $surveyModel->id = $surveyId;
+            $surveyModel->respondent_id = $request->input('respondent_id');
+            $surveyModel->form_id = $request->input('form_id');
+            $surveyModel->study_id = $request->input('study_id');
+            $surveyModel->save();
+
+            // Create new Interview
+            $interviewModel->id = $interviewId;
+            $interviewModel->survey_id = $surveyId;
+            $interviewModel->user_id = $user->id;
+            // TODO: start_time and end_time
+            $interviewModel->start_time = $now->getTimestamp();
+            $interviewModel->end_time = $now->getTimestamp();
+
+            // Iterate through questions and add edges / datum
+            foreach($request->input('questions') as $question) {
+                if ($question['question_type']['name'] == 'relationship') {
+
+                    $datumModel = $datumService->createDatum ($surveyId, $question['id'], $question['var_name'], '1');
+
+                    foreach($question['selectedRespondents'] as $respondent) {
+                        if ($respondent) {
+                            // Create edge
+                            $edgeId = Uuid::uuid4();
+                            $edgeModel = new Edge;
+                            $edgeModel->id = $edgeId;
+                            $edgeModel->source_respondent_id = $request->input('respondent_id');
+                            $edgeModel->target_respondent_id = $respondent['id'];
+                            $edgeModel->save();
+
+                            // Create edge_datum
+                            $edgeDatumId = Uuid::uuid4();
+                            $edgeDatumModel = new EdgeDatum;
+                            $edgeDatumModel->id = $edgeDatumId;
+                            $edgeDatumModel->edge_id = $edgeId;
+                            $edgeDatumModel->datum_id = $datumModel->id;
+                            $edgeDatumModel->save();
+                        }
+                    }
+                } else {
+                    $datumService->createDatum ($surveyId, $question['id'], $question['var_name'], $question['answer']);
+                }
+            }
+        });
+
+        return response()->json([
+        ], Response::HTTP_OK);
+    }
 
     /**
      * Complete the interview by setting the end_time
