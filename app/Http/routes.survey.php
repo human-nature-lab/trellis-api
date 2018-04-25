8ce2954516f5e123e4a19c3e5d1e735a871d20ae
wyattis <wyattisrael@gmail.com>
2018-04-25 11:51:48 -0400

    Added some respondent routes for the survey view

--- app/Http/routes.survey.php
+++ app/Http/routes.survey.php
@@ -14,21 +14,20 @@ $app->group([
     $app->get('form/{form_id}/show',                        'SurveyViewController@showLogin');
 
     $app->post('form/{formId}/login',                       'InterviewController@selfAdministeredLogin');
-
     $app->get('interview/{interview_id}/data',              'InterviewDataController@getInterviewData');
-
     $app->post('interview/{interview_id}/data',             'InterviewDataController@updateInterviewData');
-
     $app->get('interview/{interview_id}',                   'InterviewController@getInterview');
 
     $app->get('form/{form_id}',                             'FormController@getForm');
 
+    $app->get('study/{study_id}/respondents/search',        'RespondentController@searchRespondentsByStudyId');
+    $app->get('study/{study_id}/respondents',               'RespondentController@getAllRespondentsByStudyId');
+    $app->get('respondent/{respondent_id}',                 'RespondentController@getRespondentById');
 });
 
 
 // Old interview routes
 $app->post('form/{id}/interview/{respondentId}/submit', 'InterviewController@submit');
 $app->get('form/{id}',                                      'FormController@getForm');
-$app->get('study/{study_id}/respondents',                   'RespondentController@getAllRespondentsByStudyId');
 $app->get('study/{id}',                                     'StudyController@getStudy');
 $app->get('photo/{id}',                                     'PhotoController@getPhoto');
