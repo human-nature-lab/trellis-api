91b0134ff49d90eda1bdee004d41ca7684768b9b
Mark McKnight <mark.mcknight@yale.edu>
2018-05-18 11:09:02 -0400

    Added route and controller method for downloading snapshots.

--- app/Http/Controllers/SyncControllerV2.php
+++ app/Http/Controllers/SyncControllerV2.php
@@ -76,6 +76,42 @@ class SyncControllerV2 extends Controller
         return response()->json($size, Response::HTTP_OK);
     }
 
+    public function downloadSnapshot(Request $request, $snapshotId) {
+        $validator = Validator::make(array_merge($request->all(), [
+            'id' => $snapshotId
+        ]), [
+            'id' => 'required|string|exists:snapshot,id'
+        ]);
+
+        if ($validator->fails() === true) {
+            return response()->json([
+                'msg' => 'Validation failed',
+                'err' => $validator->errors()
+            ], $validator->statusCode());
+        }
+
+        $snapshot = Snapshot::find($snapshotId);
+        if ($snapshot === null) {
+            return response()->json([
+                'msg' => 'Snapshot ID not found.',
+                'err' => $validator->errors()
+            ], $validator->statusCode());
+        }
+
+        $adapter = new Local(storage_path() . '/snapshot');
+        $filesystem = new Filesystem($adapter);
+        $exists = $filesystem->has($snapshot->file_name);
+
+        if (!$exists) {
+            return response()->json([
+                'msg' => 'Snapshot file not found.',
+                'err' => $validator->errors()
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        return response()->download(storage_path() . '/snapshot/' . $snapshot->file_name);
+    }
+
     public function getSnapshotInfo(Request $request, $deviceId) {
         $validator = Validator::make(array_merge($request->all(), [
             'id' => $deviceId
