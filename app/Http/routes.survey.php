789931255205ad520218b778291bb18e7fb72b89
wyattis <wyattisrael@gmail.com>
2018-05-31 12:46:52 -0400

    Removed temporary routes from admin routes. Added token auth to survey routes. Added route for getting all possible respondent condition tags

--- app/Http/routes.survey.php
+++ app/Http/routes.survey.php
@@ -9,19 +9,30 @@ use Illuminate\Support\Facades\Artisan;
 
 $app->group([
     'namespace' => 'App\Http\Controllers',
-    'prefix' => 'survey-view'
+    'prefix' => 'survey-view',
+    'middleware' => ['key'],
 ], function ($app) {
-    $app->get('form/{form_id}/show',                        'SurveyViewController@showLogin');
-    $app->post('form/{formId}/login',                       'InterviewController@selfAdministeredLogin');
+    $app->post('login',                                         'TokenController@createToken');
+});
+
+$app->group([
+    'namespace' => 'App\Http\Controllers',
+    'prefix' => 'survey-view',
+    'middleware' => ['key', 'token']
+], function ($app) {
+
+    $app->get('condition-tags/respondent',                  'ConditionController@getAllRespondentConditionTags');
+
+//        $app->get('form/{form_id}/show',                        'SurveyViewController@showLogin');
+//        $app->post('form/{formId}/login',                       'InterviewController@selfAdministeredLogin');
     $app->get('form/{form_id}',                             'FormController@getForm');
 
     $app->get('interview/{interview_id}/actions',           'InterviewDataController@getInterviewActionsByInterviewId');
     $app->get('interview/{interview_id}/data',              'InterviewDataController@getInterviewDataByInterviewId');
-    $app->get('interview/{interview_id}/preload',          'PreloadController@getPreloadDataByInterviewId');
+    $app->get('interview/{interview_id}/preload',           'PreloadController@getPreloadDataByInterviewId');
     $app->get('interview/{interview_id}',                   'InterviewController@getInterview');
     $app->post('interview/{interview_id}/actions',          'InterviewDataController@saveInterviewActions');
     $app->post('interview/{interview_id}/data',             'InterviewDataController@updateInterviewData');
-
 
     $app->get('study/{study_id}/respondents/search',        'RespondentController@searchRespondentsByStudyId');
     $app->get('study/{study_id}/respondents',               'RespondentController@getAllRespondentsByStudyId');
@@ -46,6 +57,6 @@ $app->group([
 
 // Old interview routes
 $app->post('form/{id}/interview/{respondentId}/submit', 'InterviewController@submit');
-$app->get('form/{id}',                                      'FormController@getForm');
-$app->get('study/{id}',                                     'StudyController@getStudy');
-$app->get('photo/{id}',                                     'PhotoController@getPhoto');
+$app->get('form/{id}',                                  'FormController@getForm');
+$app->get('study/{id}',                                 'StudyController@getStudy');
+$app->get('photo/{id}',                                 'PhotoController@getPhoto');
