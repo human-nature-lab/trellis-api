070fd706aa0385a9982d61e8a43013d6847c3427
wyattis <wyattisrael@gmail.com>
2018-06-18 15:43:27 -0400

    fixed question_datum and action migrations.

--- database/migrations/2018_03_27_153447_action_and_datum_and_question_datum.php
+++ database/migrations/2018_03_27_153447_action_and_datum_and_question_datum.php
@@ -19,8 +19,7 @@ class ActionAndDatumAndQuestionDatum extends Migration
             $table->integer('section_repetition');
             $table->string('follow_up_datum_id', 41)->nullable()->index('fk__question_datum__datum_idx');
             $table->string('question_id', 41)->index('fk__question_datum__question_idx');
-            $table->string('survey_id', 41)->nullable()->index('fk__question_datum__survey_idx');
-            $table->string('preload_id', 41)->nullable()->index('fk__question_datum__preload_idx');
+            $table->string('interview_id', 41)->nullable()->index('fk__question_datum__interview_idx');
 
             $table->dateTime('answered_at')->nullable();
             $table->dateTime('skipped_at')->nullable();
@@ -34,6 +33,7 @@ class ActionAndDatumAndQuestionDatum extends Migration
         Schema::table('question_datum', function (Blueprint $table) {
             $table->foreign('follow_up_datum_id')->references('id')->on('datum')->onUpdate('no action')->onDelete('set null');
             $table->foreign('question_id')->references('id')->on('question')->onUpdate('no action')->onDelete('cascade');
+            $table->foreign('interview_id')->references('id')->on('interview')->onUpdate('no action')->onDelete('set null');
         });
 
         Schema::table('action', function (Blueprint $table) {
