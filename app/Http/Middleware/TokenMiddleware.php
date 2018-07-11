ff5eba2b65f81c3427018c73f939d2043326af5f
wyattis <wyattisrael@gmail.com>
2018-07-11 11:11:00 -0400

    extend token life on any request made to the application

--- app/Http/Middleware/TokenMiddleware.php
+++ app/Http/Middleware/TokenMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Carbon\Carbon;
 use Closure;
 use DB;
 use Illuminate\Http\Response;
@@ -20,13 +21,16 @@ class TokenMiddleware
         $token = $request->headers->get('X-Token');
 
         $tokenModel = Token::where('token_hash', $token)
-            ->where('created_at', '>=', DB::raw('now() - interval '.$_ENV['TOKEN_EXPIRE'].' minute'))
+            ->where('updated_at', '>=', DB::raw('now() - interval '.$_ENV['TOKEN_EXPIRE'].' minute'))
             ->first();
 
         if ($tokenModel === null) {
             return response()->json([
                 'msg' => Response::$statusTexts[Response::HTTP_UNAUTHORIZED]
             ], Response::HTTP_UNAUTHORIZED);
+        } else {
+            $tokenModel->updated_at = Carbon::now();
+            $tokenModel->save();
         }
 
         return $next($request);
