96789b5edcb2561c48c488480121612576da24a6
Mark McKnight <mark.mcknight@yale.edu>
2018-09-27 16:14:05 -0400

    Added routes and controller methods for listing missing images and uploading images. Started new command for processing uploads.

---
+++ config/upload.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'directory' => [
+        'path' => env('UPLOAD_DIRECTORY_PATH', 'uploads')
+    ],
+    'seconds' => [
+        'min' => env('UPLOAD_SECONDS_MIN', 60),
+    ]
+];
