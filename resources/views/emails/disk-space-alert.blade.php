f5d71176429b5d65466b5a6d7c34807e3312e706
wyattis <wyattisrael@gmail.com>
2019-05-17 14:20:57 -0400

    Added commands to clean out reports and check disk space. Added a schedule to periodically do these things. Must be enabled by adding a cron job for the laravel scheduler.

---
+++ resources/views/emails/disk-space-alert.blade.php
@@ -0,0 +1,9 @@
+<body>
+    <p>The server hosting {{$site}} is running out of disk space!</p>
+    <p>Disk size: {{$total}}MB</p>
+    <p>Free space: {{$free}}MB</p>
+    <p>Configured minimum space: {{$min}}MB</p>
+    <p>Please clean out large files or increase the amount of disk space to avoid errors in the future.</p>
+    <br>
+    <p>The Trellis Team</p>
+</body>
