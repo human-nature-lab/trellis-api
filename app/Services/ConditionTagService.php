0236ec713432a54068ed5c18adfc75b34befa7f5
wyattis <wyattisrael@gmail.com>
2019-06-11 16:16:01 -0400

    Added support for condition tag imports and respondent location importing. Also added middleware to set cache headers.

--- app/Services/ConditionTagService.php
+++ app/Services/ConditionTagService.php
@@ -4,8 +4,8 @@ namespace app\Services;
 
 use App\Library\CsvFileReader;
 use App\Models\ConditionTag;
-use App\Models\Respondent;
 use App\Models\RespondentConditionTag;
+use App\Services\RespondentService;
 use Ramsey\Uuid\Uuid;
 use DB;
 use Log;
@@ -29,6 +29,7 @@ class ConditionTagService {
       $respondentConditionTag->id = Uuid::uuid4();
       $respondentConditionTag->condition_tag_id = $conditionTag->id;
       $respondentConditionTag->respondent_id = $respondentId;
+      $respondentConditionTag->save();
       return $respondentConditionTag;
     }
 
@@ -40,23 +41,18 @@ class ConditionTagService {
         $csv = new CsvFileReader($filePath);
         $csv->open();
         $line = $csv->getNextRowHash();
-        while (isset($line)) {
-          Log::info(json_encode($line));
-          $line = $csv->getNextRowHash();
+        while ($line) {
           $respondentAssignedId = trim($line['respondent_id']);
           $conditionTagName = trim($line['condition_tag']);
           $conditionTag = ConditionTagService::createConditionTag($conditionTagName);
-          $respondent = Respondent::whereIn('id', function ($q) use ($studyId) {
-            return $q->select('respondent_id')->from('study_respondent')->where('study_id', $studyId);
-          })->where(function ($q) use ($respondentAssignedId) {
-            return $q->where('id', $respondentAssignedId)->orWhere('assigned_id', $respondentAssignedId);
-          })->first();
+          $respondent = RespondentService::lookupRespondentById($respondentAssignedId, $studyId);
           if (!isset($respondent)) {
             throw new Exception("Unable to find respondent with id matching $respondentAssignedId");
           }
           $rct = $this->createRespondentConditionTag($respondent->id, $conditionTag);
           array_push($importedRCTIds, $rct->id);
           $row++;
+          $line = $csv->getNextRowHash();
         }
       } finally {
         $csv->close();
