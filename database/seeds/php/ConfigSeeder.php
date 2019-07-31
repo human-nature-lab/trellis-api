41a613c0fca5707abcc92de2749bb15d257c9a2f
wyattis <wyattisrael@gmail.com>
2019-07-31 12:43:21 -0400

    Update the default value for form builder config url

--- database/seeds/php/ConfigSeeder.php
+++ database/seeds/php/ConfigSeeder.php
@@ -13,7 +13,7 @@ class ConfigSeeder extends Seeder {
         'appMode' => 'PROD',
         'siteName' => 'Trellis Demo',
         'webRoot' => 'trellisdemo.net',
-        'formBuilderUrl' => '/form-builder/#/form/{form_id}/builder?token={token}&study={study}&locale={locale}&apiRoot={apiRoot}',   // The url template to use for the form builder iframe
+        'formBuilderUrl' => '/form-builder/index.html#/form/{form_id}/builder?token={token}&study={study}&locale={locale}&apiRoot={apiRoot}',   // The url template to use for the form builder iframe
         'mapTileLayer.url' => 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}', // The url template to use for the mapbox layer
         'mapTileLayer.attribution' => 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         'mapTileLayer.maxZoom' => 20,
