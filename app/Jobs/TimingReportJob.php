3316276c6d8d45b2815427f18b1ab5590741f751
wyattis <wyattisrael@gmail.com>
2019-09-06 21:42:03 -0400

    Reduced average memory usage of TimingReportJob

--- app/Jobs/TimingReportJob.php
+++ app/Jobs/TimingReportJob.php
@@ -4,11 +4,13 @@ namespace App\Jobs;
 
 
 use App\Classes\CsvFileStream;
-use App\Models\QuestionDatum;
+use App\Models\Interview;
+use App\Models\QuestionType;
+use App\Models\User;
 use App\Services\ReportService;
+use Illuminate\Support\Facades\DB;
 use Log;
 use App\Models\Report;
-use App\Models\Datum;
 use App\Models\Study;
 use Ramsey\Uuid\Uuid;
 
@@ -81,35 +83,61 @@ class TimingReportJob extends Job
 
         $study = Study::find($this->studyId);
 
-        $q = QuestionDatum::join('survey', 'survey.id', '=', 'question_datum.survey_id')
-            ->join('interview', 'interview.survey_id', '=', 'survey.id')
-            ->join('user', 'interview.user_id', '=', 'user.id')
-            ->join('question', 'question.id', '=', 'question_datum.question_id')
-            ->join('question_type', 'question.question_type_id', '=', 'question_type.id')
-            ->where('survey.study_id', '=', $study->id)
-            // ->orderBy('survey.respondent_id')
-            ->select(
-                "interview.id as interview_id",
-                "question_datum.survey_id",
-                'question_datum.question_id',
-                "survey.form_id",
-                "survey.respondent_id",
-                "survey.study_id",
-                "interview.user_id",
-                "user.name",
-                "user.username",
-                "question.var_name as question_name",
-                "question_type.name as question_type",
-                'question_datum.created_at',
-                'question_datum.updated_at',
-                'question_datum.deleted_at');
+        $questionTypes = QuestionType::all()->reduce(function ($agg, $qt) {
+          $agg[$qt->id] = $qt->name;
+          return $agg;
+        }, []);
 
-        //Log::debug($q->toSql());
-      
-        foreach ($q->cursor() as $datum) {
-            $datum = $datum->toArray();
-            $this->file->writeRow($datum);
-        }
+        $users = User::all()->reduce(function ($agg, $user) {
+          $agg[$user->id] = $user;
+          return $agg;
+        }, []);
+
+        $interviews = Interview::join('survey', 'interview.survey_id', '=', 'survey.id')
+          ->where('survey.study_id', '=', $study->id);
+        $interviews->chunk(200, function ($interviews) use ($users, $questionTypes) {
+          $interviewToSurveyMap = [];
+          $interviewMap = [];
+          $surveyIds = [];
+          foreach ($interviews as $interview) {
+            array_push($surveyIds, $interview->survey_id);
+            $interviewToSurveyMap[$interview->id] = $interview->survey_id;
+            $interviewMap[$interview->survey_id] = $interview;
+          }
+          $questionData = DB::table('question_datum')
+            ->join('question', 'question_datum.question_id', '=', 'question.id')
+            ->whereIn('survey_id', $surveyIds)
+            ->select(
+              'question.var_name',
+              'question.question_type_id',
+              'question_id',
+              'question_datum.survey_id',
+              'question_datum.created_at',
+              'question_datum.updated_at',
+              'question_datum.deleted_at'
+            )->get();
+          foreach ($questionData as $qd) {
+            $interview = $interviewMap[$qd->survey_id];
+            $user = $users[$interview->user_id];
+            $questionTypeName = $questionTypes[$qd->question_type_id];
+            $this->file->writeRow([
+              'interview_id' => $interview->id,
+              'survey_id' => $interview->survey_id,
+              'question_id' => $qd->question_id,
+              'form_id' => $interview->form_id,
+              'respondent_id' => $interview->respondent_id,
+              'study_id' => $interview->study_id,
+              'user_id' => $user->id,
+              'name' => $user->name,
+              'username' => $user->username,
+              'question_name' => $qd->var_name,
+              'question_type' => $questionTypeName,
+              'created_at' => $qd->created_at,
+              'updated_at' => $qd->updated_at,
+              'deleted_at' => $qd->deleted_at
+            ]);
+          }
+        });
 
         ReportService::saveFileStream($this->report, $fileName);
 
