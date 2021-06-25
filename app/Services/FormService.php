6752b7eb3fa70e921f20635a074c49b9ce34809c
wyattis <wyattisrael@gmail.com>
2021-06-25 12:55:41 -0600

    always create a new form as version 0

--- app/Services/FormService.php
+++ app/Services/FormService.php
@@ -3,259 +3,190 @@
 namespace App\Services;
 
 use App\Models\Form;
-use App\Models\Section;
 use App\Models\StudyForm;
 use App\Models\Study;
 use App\Models\TranslationText;
 use App\Models\Translation;
-use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Log;
-use DB;
+use Illuminate\Support\Facades\DB;
 use Ramsey\Uuid\Uuid;
 use Throwable;
 
-class FormService
-{
-    public static function getAllForms()
-    {
+class FormService {
+
+  public static function getAllStudyForms($studyId) {
+    $forms = Form::where('study.id', $studyId)
+      ->leftJoin('study_form', 'study_form.id', '=', 'form.id')
+      ->join('study', 'study.id', '=', 'study_form.study_id')
+      ->get();
+
+    return $forms;
+  }
+
+  public static function getAllStudyFormsPaginated($perPage, $studyId) {
+    $forms = Form::select('tt.translated_text AS name', 'f.version', 'f.updated_at', 'f.id')
+      ->from('translation_text AS tt')
+      ->join('form AS f', 'tt.translation_id', '=', 'f.name_translation_id')
+      ->join('study_form AS sf', 'sf.form_master_id', '=', 'f.form_master_id')
+      ->where('sf.study_id', $studyId)
+      ->orderBy('f.created_at')
+      ->paginate($perPage);
+
+    return $forms;
+  }
+
+  public static function importFormAndAddToStudy($filePath, $formName, $studyId, $formType) {
+    $study = Study::find($studyId);
+    $importedForm = self::importFormFromPath($filePath, $formName, $study->default_locale_id);
+    self::createStudyForm($importedForm->form_master_id, $studyId, $formType);
+    return $importedForm;
+  }
+
+  public static function createStudyForm($formId, $studyId, $formType) {
+    $studyForm = new StudyForm;
+    $studyForm->id = Uuid::uuid4();
+    $studyForm->study_id = $studyId;
+    $studyForm->form_master_id = $formId;
+
+    $formTypeId = 1 * $formType;
+    $maxSortOrder = DB::table('study_form')
+      ->where('study_id', '=', $studyId)
+      ->where('form_type_id', '=', $formTypeId)
+      ->whereNull('deleted_at')
+      ->max('sort_order');
+
+    if ($maxSortOrder == null) {
+      $maxSortOrder = 0;
     }
 
-    public static function getAllStudyForms($studyId)
-    {
-        $forms = Form::where('study.id', $studyId)
-            ->leftJoin('study_form', 'study_form.id', '=', 'form.id')
-            ->join('study', 'study.id', '=', 'study_form.study_id')
-            ->get();
+    $studyForm->sort_order = $maxSortOrder + 1;
 
-        return $forms;
-    }
+    $studyForm->form_type_id = $formTypeId;
 
-    public static function getAllStudyFormsPaginated($perPage, $studyId)
-    {
-        $forms = Form::select('tt.translated_text AS name', 'f.version', 'f.updated_at', 'f.id')
-            ->from('translation_text AS tt')
-            ->join('form AS f', 'tt.translation_id', '=', 'f.name_translation_id')
-            ->join('study_form AS sf', 'sf.form_master_id', '=', 'f.form_master_id')
-            ->where('sf.study_id', $studyId)
-            ->orderBy('f.created_at')
-            ->paginate($perPage);
+    $studyForm->save();
+    return $studyForm;
+  }
 
-        return $forms;
-    }
+  public static function importFormFromPath($filePath, $formName, $localeId) {
+    $oldQuestionIdToNewQuestionIdMap = []; // Used for follow up questions
 
-    public static function importFormAndAddToStudy ($filePath, $formName, $studyId, $formType) {
-        $study = Study::find($studyId);
-        $importedForm = self::importFormFromPath($filePath, $formName, $study->default_locale_id);
-        self::createStudyForm($importedForm->form_master_id, $studyId, $formType);
-        return $importedForm;
-    }
+    // Create the form
+    $importedForm = self::createForm($formName, $localeId);
+    $importedFormId = $importedForm->form_master_id;
+    $formJsonString = file_get_contents($filePath);
+    $jsonObject = json_decode($formJsonString, true);
+    $formObject = $jsonObject["form"];
 
-    public static function createStudyForm ($formId, $studyId, $formType) {
-        $studyForm = new StudyForm;
-        $studyForm->id = Uuid::uuid4();
-        $studyForm->study_id = $studyId;
-        $studyForm->form_master_id = $formId;
+    // It is essential to insert in the same order that the form is conducted in so that follow up questions exist
+    // before their follow up sections
+    uasort($formObject["sections"], function ($a, $b) {
+      return $a['form_sections'][0]["sort_order"] - $b['form_sections'][0]["sort_order"];
+    });
 
-        $formTypeId = 1 * $formType;
-        $maxSortOrder = DB::table('study_form')
-            ->where('study_id', '=', $studyId)
-            ->where('form_type_id', '=', $formTypeId)
-            ->whereNull('deleted_at')
-            ->max('sort_order');
+    foreach ($formObject["sections"] as $sectionObject) {
+      $sectionSortOrder = $sectionObject["form_sections"][0]["sort_order"];
+      $maxRepetitions = $sectionObject['form_sections'][0]['max_repetitions'];
+      $isRepeatable = $sectionObject['form_sections'][0]['is_repeatable'];
 
-        if ($maxSortOrder == null) {
-            $maxSortOrder = 0;
+      // This will fail if the sections with follow up questions aren't inserted after the referenced questions
+      $newFollowUpQuestionId = $sectionObject['form_sections'][0]['follow_up_question_id'] ? $oldQuestionIdToNewQuestionIdMap[$sectionObject['form_sections'][0]['follow_up_question_id']] : null;
+
+      $sectionNameTranslationId = TranslationService::importTranslation($sectionObject["name_translation"]);
+      $importedSection = SectionService::createTranslatedSection($importedFormId, $sectionNameTranslationId, $sectionSortOrder, $newFollowUpQuestionId, $isRepeatable, $maxRepetitions);
+
+      foreach ($sectionObject["question_groups"] as $questionGroupObject) {
+        $questionGroupSortOrder = $questionGroupObject["pivot"]["question_group_order"];
+        $importedQuestionGroup = QuestionGroupService::createQuestionGroup($importedSection["id"], $questionGroupSortOrder);
+
+        foreach ($questionGroupObject["skips"] as $skipObject) {
+          $importedSkip = SkipService::createSkip($importedQuestionGroup["id"], $skipObject["show_hide"], $skipObject["any_all"], $skipObject["precedence"]);
+
+          foreach ($skipObject["conditions"] as $skipConditionTagObject) {
+            SkipService::createSkipConditionTag($importedSkip["id"], $skipConditionTagObject["condition_tag_name"]);
+          }
         }
 
-        $studyForm->sort_order = $maxSortOrder + 1;
+        foreach ($questionGroupObject["questions"] as $questionObject) {
+          $questionTranslationId = TranslationService::importTranslation($questionObject["question_translation"]);
+          $importedQuestion = QuestionService::createTranslatedQuestion($importedQuestionGroup["id"], $questionTranslationId, $questionObject["var_name"], $questionObject["question_type"]["id"], $questionObject["sort_order"]);
+          $oldQuestionIdToNewQuestionIdMap[$questionObject['id']] = $importedQuestion['id'];
+          foreach ($questionObject["question_parameters"] as $questionParameterObject) {
+            QuestionParameterService::createQuestionParameter($importedQuestion["id"], $questionParameterObject["parameter_id"], $questionParameterObject["val"]);
+          }
 
-        $studyForm->form_type_id = $formTypeId;
+          foreach ($questionObject["assign_condition_tags"] as $assignConditionTagObject) {
+            $importedConditionTag = ConditionTagService::createConditionTag($assignConditionTagObject['condition']['name']);
+            AssignConditionTagService::createAssignConditionTag($importedQuestion["id"], $importedConditionTag["id"], $assignConditionTagObject["logic"], $assignConditionTagObject["scope"]);
+          }
 
-        $studyForm->save();
-        return $studyForm;
-    }
-
-    public static function importFormFromPath ($filePath, $formName, $localeId) {
-        $oldQuestionIdToNewQuestionIdMap = []; // Used for follow up questions
-
-        // Create the form
-        $importedForm = self::createForm($formName, $localeId);
-        $importedFormId = $importedForm->form_master_id;
-        $formJsonString = file_get_contents($filePath);
-        $jsonObject = json_decode($formJsonString, true);
-        $formObject = $jsonObject["form"];
-
-        // It is essential to insert in the same order that the form is conducted in so that follow up questions exist
-        // before their follow up sections
-        uasort($formObject["sections"], function ($a, $b) {
-            return $a['form_sections'][0]["sort_order"] - $b['form_sections'][0]["sort_order"];
-        });
-
-        foreach ($formObject["sections"] as $sectionObject) {
-            $sectionSortOrder = $sectionObject["form_sections"][0]["sort_order"];
-            $maxRepetitions = $sectionObject['form_sections'][0]['max_repetitions'];
-            $isRepeatable = $sectionObject['form_sections'][0]['is_repeatable'];
-
-            // This will fail if the sections with follow up questions aren't inserted after the referenced questions
-            $newFollowUpQuestionId = $sectionObject['form_sections'][0]['follow_up_question_id'] ? $oldQuestionIdToNewQuestionIdMap[$sectionObject['form_sections'][0]['follow_up_question_id']] : null;
-
-            $sectionNameTranslationId = TranslationService::importTranslation($sectionObject["name_translation"]);
-            $importedSection = SectionService::createTranslatedSection($importedFormId, $sectionNameTranslationId, $sectionSortOrder, $newFollowUpQuestionId, $isRepeatable, $maxRepetitions);
-
-            foreach ($sectionObject["question_groups"] as $questionGroupObject) {
-                $questionGroupSortOrder = $questionGroupObject["pivot"]["question_group_order"];
-                $importedQuestionGroup = QuestionGroupService::createQuestionGroup($importedSection["id"], $questionGroupSortOrder);
-
-                foreach ($questionGroupObject["skips"] as $skipObject) {
-                    $importedSkip = SkipService::createSkip($importedQuestionGroup["id"], $skipObject["show_hide"], $skipObject["any_all"], $skipObject["precedence"]);
-
-                    foreach ($skipObject["conditions"] as $skipConditionTagObject) {
-                        SkipService::createSkipConditionTag($importedSkip["id"], $skipConditionTagObject["condition_tag_name"]);
-                    }
-                }
-
-                foreach ($questionGroupObject["questions"] as $questionObject) {
-                    $questionTranslationId = TranslationService::importTranslation($questionObject["question_translation"]);
-                    $importedQuestion = QuestionService::createTranslatedQuestion($importedQuestionGroup["id"], $questionTranslationId, $questionObject["var_name"], $questionObject["question_type"]["id"], $questionObject["sort_order"]);
-                    $oldQuestionIdToNewQuestionIdMap[$questionObject['id']] = $importedQuestion['id'];
-                    foreach ($questionObject["question_parameters"] as $questionParameterObject) {
-                        QuestionParameterService::createQuestionParameter($importedQuestion["id"], $questionParameterObject["parameter_id"], $questionParameterObject["val"]);
-                    }
-
-                    foreach ($questionObject["assign_condition_tags"] as $assignConditionTagObject) {
-                        $importedConditionTag = ConditionTagService::createConditionTag($assignConditionTagObject['condition']['name']);
-                        AssignConditionTagService::createAssignConditionTag($importedQuestion["id"], $importedConditionTag["id"], $assignConditionTagObject["logic"], $assignConditionTagObject["scope"]);
-                    }
-
-                    foreach ($questionObject["choices"] as $choiceObject) {
-                        $choiceTranslationId = TranslationService::importTranslation($choiceObject['choice_translation']);
-                        QuestionChoiceService::createTranslatedQuestionChoice($importedQuestion["id"], $choiceTranslationId, $choiceObject["val"], $choiceObject["pivot"]["sort_order"]);
-                    }
-                }
-            }
+          foreach ($questionObject["choices"] as $choiceObject) {
+            $choiceTranslationId = TranslationService::importTranslation($choiceObject['choice_translation']);
+            QuestionChoiceService::createTranslatedQuestionChoice($importedQuestion["id"], $choiceTranslationId, $choiceObject["val"], $choiceObject["pivot"]["sort_order"]);
+          }
         }
-
-        return $importedForm;
+      }
     }
 
-    public static function createForm($formName, $localeId) {
-        $newFormModel = null;
+    return $importedForm;
+  }
 
-        try {
-            DB::beginTransaction();
-            $translationId = Uuid::uuid4();
-            $translationTextId = Uuid::uuid4();
-            $formId = Uuid::uuid4()->toString();
+  public static function createForm($formName, $localeId) {
+    $newFormModel = null;
 
-            // Create new Translation.
-            $newTranslationModel = new Translation;
-            $newTranslationModel->id = $translationId;
-            $newTranslationModel->save();
+    try {
+      DB::beginTransaction();
+      $translationId = Uuid::uuid4();
+      $translationTextId = Uuid::uuid4();
+      $formId = Uuid::uuid4()->toString();
 
-            // Create new TranslationText.
-            $newTranslationTextModel = new TranslationText;
-            $newTranslationTextModel->id = $translationTextId;
-            $newTranslationTextModel->translation_id = $translationId;
-            $newTranslationTextModel->locale_id = $localeId;
-            $newTranslationTextModel->translated_text = $formName;
-            $newTranslationTextModel->save();
+      // Create new Translation.
+      $newTranslationModel = new Translation;
+      $newTranslationModel->id = $translationId;
+      $newTranslationModel->save();
 
-            $formMasterId = $formId;
+      // Create new TranslationText.
+      $newTranslationTextModel = new TranslationText;
+      $newTranslationTextModel->id = $translationTextId;
+      $newTranslationTextModel->translation_id = $translationId;
+      $newTranslationTextModel->locale_id = $localeId;
+      $newTranslationTextModel->translated_text = $formName;
+      $newTranslationTextModel->save();
 
-            // Set Version.
-            $version = Form::where('form_master_id', '=', $formMasterId)
-                ->max('version');
+      $formMasterId = $formId;
 
-            if ($version !== null) {
-                $version++;
-                $formVersion = $version;
-            } else {
-                $formVersion = 1;
-            }
+      // Create new Form.
+      $newFormModel = Form::create([
+        'id' => $formId,
+        'form_master_id' => $formMasterId,
+        'name_translation_id' => $translationId,
+        'version' => 0
+      ]);
 
-            // Create new Form.
-            $newFormModel = Form::create([
-                'id' => $formId,
-                'form_master_id' => $formMasterId,
-                'name_translation_id' => $translationId,
-                'version' => $formVersion
-            ]);
-
-            DB::commit();
-
-        } catch (Throwable $e) {
-            Log::error($e);
-            DB::rollBack();
-        }
-
-        return $newFormModel;
+      DB::commit();
+    } catch (Throwable $e) {
+      Log::error($e);
+      DB::rollBack();
     }
 
-    /**
-     * Creates both a form entry and a study_form entry within a transaction
-     * @param $formName
-     * @param $studyId
-     * @param $formType
-     * @return null
-     */
-    public static function createFormWithStudyForm ($formName, $studyId, $formType) {
-        $form = null;
+    return $newFormModel;
+  }
 
-        DB::transaction(function () use ($formName, $studyId, $formType, &$form) {
-            $study = Study::find($studyId);
-            $form = self::createForm($formName, $study->default_locale_id);
-            $studyForm = self::createStudyForm($form->id, $studyId, $formType);
-        });
+  /**
+   * Creates both a form entry and a study_form entry within a transaction
+   * @param $formName
+   * @param $studyId
+   * @param $formType
+   * @return null
+   */
+  public static function createFormWithStudyForm($formName, $studyId, $formType) {
+    $form = null;
 
-        return $form;
-    }
+    DB::transaction(function () use ($formName, $studyId, $formType, &$form) {
+      $study = Study::find($studyId);
+      $form = self::createForm($formName, $study->default_locale_id);
+      $studyForm = self::createStudyForm($form->id, $studyId, $formType);
+    });
 
-    /*
-    public static function createNewForm($request, $studyId, $translationId = null)
-    {
-        // Check for existing translation record.
-        if ($translationId == null) {
-            // Insert Translation record.
-            $translation = new Translation();
-
-            $translationId = Uuid::uuid4();
-            $translation->id = $translationId;
-
-            $translation->save();
-        }
-
-        // Insert Translation_Text record.
-        $translationText = new TranslationText();
-
-        $translationText->id = Uuid::uuid4();
-        $translationText->translation_id = $translationId;
-        $translationText->locale_id = $request->input('locale_id');
-        $translationText->translated_text = $request->input('name');
-
-        $translationText->save();
-
-        // Insert Form record.
-        $form = new Form();
-
-        $formId = Uuid::uuid4();
-        $form->id = $formId;
-        $form->form_master_id = $form->id;
-        $form->name_translation_id = $translationId;
-        $form->version = '1';
-
-        $form->save();
-
-        // Insert Study-Form record.
-        $studyForm = new StudyForm();
-
-        $studyForm->id = Uuid::uuid4();
-        $studyForm->study_id = $studyId;
-        $studyForm->form_master_id = $formId;
-        $studyForm->sort_order = 0;
-
-        $studyForm->save();
-
-        return $form;
-    }
-    */
+    return $form;
+  }
 }
