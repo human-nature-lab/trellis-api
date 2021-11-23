218c0552a6808750cec00b3b6137d278d9d738d6
wyattis <wyattisrael@gmail.com>
2021-11-23 11:47:22 -0700

    action preloading works

--- app/Library/CsvFileReader.php
+++ app/Library/CsvFileReader.php
@@ -9,19 +9,19 @@ class CsvFileReader {
 
   private $file;
   private $path;
-  private $headers;
+  public $headers;
   public $isOpen = false;
   public $makePath;
 
-  public function __construct ($path, $makePath = false, $headers = null) {
+  public function __construct($path, $makePath = false, $headers = null) {
     $this->path = $path;
     $this->headers = $headers;
     $this->makePath = $makePath;
   }
 
 
-  public function open () {
-    ini_set('auto_detect_line_endings',TRUE);
+  public function open() {
+    ini_set('auto_detect_line_endings', TRUE);
     $existing = file_exists(dirname($this->path));
     if (!$existing) {
       if ($this->makePath) {
@@ -39,42 +39,42 @@ class CsvFileReader {
     }
 
     $this->isOpen = true;
-
   }
 
-  public function close () {
-
+  public function close() {
     if (isset($this->file)) {
       fclose($this->file);
     }
-
     $this->isOpen = false;
   }
 
-  public function getNextRowHash () {
-
+  public function getNextRowHash() {
     $raw = $this->getNextRow();
-
-    Log::debug(gettype($raw));
-    Log::debug($raw ? 'TRUE' : 'FALSE');
 
     if (!$raw) return $raw;
 
     $row = [];
 
     foreach ($this->headers as $i => $key) {
-      $row[$key] = $raw[$i];
+      if ($raw[$i] !== '' && $raw[$i] !== null) {
+        $row[$key] = $raw[$i];
+      }
     }
 
     return $row;
   }
 
-  public function getNextRow () {
-
+  public function getNextRow() {
     if (!$this->isOpen) throw new Error('This file has not been opened');
-
     return fgetcsv($this->file);
-
   }
-
+  
+  public function hasHeaders ($headers): bool {
+    foreach ($headers as $header) {
+      if (!in_array($header, $this->headers)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
--- app/Services/TranslationService.php
+++ app/Services/TranslationService.php
@@ -5,47 +5,83 @@ namespace App\Services;
 use App\Models\Study;
 use App\Models\Translation;
 use App\Models\TranslationText;
+use App\Models\Locale;
 use Ramsey\Uuid\Uuid;
 
-class TranslationService
-{
-    public static function createNewTranslation () {
-        $translationId = Uuid::uuid4();
-        $translation = new Translation();
+class TranslationService {
+  public static function createNewTranslation() {
+    $translationId = Uuid::uuid4();
+    $translation = new Translation();
 
-        $translation->id = $translationId;
+    $translation->id = $translationId;
 
-        $translation->save();
+    $translation->save();
 
-        return $translationId;
+    return $translationId;
+  }
+
+  static function createTranslationForDefault(String $text, Study $study): Translation {
+    $translation = new Translation;
+    $translation->id = Uuid::uuid4();
+    $translation->save();
+    $translationText = new TranslationText;
+    $translationText->id = Uuid::uuid4();
+    $translationText->translation_id = $translation->id;
+    $translationText->translated_text = $text;
+    $translationText->locale_id = $study->default_locale_id;
+    $translationText->save();
+    return $translation;
+  }
+
+  public static function importTranslation($translationObject) {
+    $translationId = Uuid::uuid4();
+    $translation = new Translation();
+    $translation->id = $translationId;
+    $translation->save();
+
+    foreach ($translationObject['translation_text'] as $translationTextObject) {
+      $translatedText = $translationTextObject['translated_text'];
+      $localeId = $translationTextObject['locale_id'];
+      TranslationTextService::createTranslationText($translationId, $translatedText, $localeId);
     }
 
-    static function createTranslationForDefault (String $text, Study $study): Translation {
-      $translation = new Translation;
-      $translation->id = Uuid::uuid4();
-      $translation->save();
-      $translationText = new TranslationText;
-      $translationText->id = Uuid::uuid4();
-      $translationText->translation_id = $translation->id;
-      $translationText->translated_text = $text;
-      $translationText->locale_id = $study->default_locale_id;
-      $translationText->save();
-      return $translation;
+    return $translationId;
+  }
+
+  /**
+   * Create a translation from an array with definitions like "geo.translation.en" and "respondent.translation.es"
+   */
+  public static function createTranslationFromArray(Array $row, String $prefix = ''): Translation {
+    $translations = [];
+    foreach ($row as $key => $item) {
+      $key = str_replace($prefix, '', $key);
+      if (preg_match('/translation\.*/', $key) === 1) {
+        $languageCode = str_replace('translation.', '', $key);
+        $translations[$languageCode] = $item;
+      }
     }
-
-    public static function importTranslation($translationObject)
-    {
-        $translationId = Uuid::uuid4();
-        $translation = new Translation();
-        $translation->id = $translationId;
-        $translation->save();
-
-        foreach ($translationObject['translation_text'] as $translationTextObject) {
-            $translatedText = $translationTextObject['translated_text'];
-            $localeId = $translationTextObject['locale_id'];
-            TranslationTextService::createTranslationText($translationId, $translatedText, $localeId);
-        }
-
-        return $translationId;
+    if (count($translations) === 0) {
+      throw new \Exception("No translations found in array");
     }
+    $translation = Translation::create([
+      'id' => Uuid::uuid4(),
+    ]);
+
+    // Create a translation_text for each locale supplied
+    foreach ($translations as $code => $text) {
+      $locale = Locale::where('language_tag', $code)
+        ->orWhere('language_name', 'like', $code)
+        ->first();
+      if (!isset($locale)) {
+        throw new \Exception("No locale for '$code' found");
+      }
+      $text = TranslationText::create([
+        'id' => Uuid::uuid4(),
+        'locale_id' => $locale->id,
+        'translated_text' => $text,
+        'translation_id' => $translation->id,
+      ]);
+    }
+    return $translation;
+  }
 }
