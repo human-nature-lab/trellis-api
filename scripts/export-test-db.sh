de235b992e126cc8d4f427385c6e5a1679ce0027
wyattis <wyattisrael@gmail.com>
2025-09-19 12:47:30 -0400

    now supporting page and section level randomization

--- scripts/export-test-db.sh
+++ scripts/export-test-db.sh
@@ -7,5 +7,5 @@ delete_file="$(dirname "$0")/delete_data.sql"
 mysql="docker-compose exec -T trellis_db mysql -uroot -ptestpass"
 mysqldump="docker-compose exec -T trellis_db mysqldump -uroot -ptestpass"
 
-# $mysql test < $delete_file
+$mysql test < $delete_file
 $mysqldump --default-character-set=utf8mb4 --skip-set-charset test > $db_file
